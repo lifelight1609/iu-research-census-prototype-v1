c353015005f8fe075be17b8b621e871919d7f9e9
Jeevan Deep Mankar <jmankar@iu.edu>
2026-03-28 19:38:13 -0500

    Update app title to IU Researchers portal

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,9 +7,9 @@ const _geist = Geist({ subsets: ["latin"] });
 const _geistMono = Geist_Mono({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: 'IU Researchers',
+  title: 'IU Researchers portal',
   description: 'Discover Indiana University researchers and their publications',
-  generator: 'IU Researchers',
+  generator: 'IU Researchers portal',
   icons: {
     icon: [
       {
